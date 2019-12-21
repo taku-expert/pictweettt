44437c7e2465595107efc4f2121ee581c443227c
taku-expert <prime.stkvkp@gmail.com>
2019-12-21 23:34:07 +0900

    test code修正後

--- spec/models/sample_spec.rb
+++ spec/models/sample_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe "User" do
   it "is invalid without a nickname" do
-    user = build(:user, nickname "")
+    user = build(:user, nickname: "")
     user.valid?
     expect(user.errors[:nickname]).to include("can't be blank")
   end
